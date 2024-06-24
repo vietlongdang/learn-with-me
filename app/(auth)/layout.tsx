9cf3b25c02740816875563aa125fca2cfacebab9
Alex Dang <lesadang1@gmail.com>
2024-06-24 11:20:50 -0500

    add authentication

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+	return <div className='flex h-full items-center justify-center'>{children}</div>
+}
